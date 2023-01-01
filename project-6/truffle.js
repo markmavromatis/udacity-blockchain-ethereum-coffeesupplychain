bb995a66db723279cff252cce84c106cff158993
Mark Mavromatis <markmavromatis@gmail.com>
2022-12-31 23:35:03 -0600

    Added Ganache network to Truffle settings

--- project-6/truffle.js
+++ project-6/truffle.js
@@ -2,7 +2,12 @@ module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
-      port: 8545,
+      port: 9545,
+      network_id: "*" // Match any network id
+    },
+    ganache: {
+      host: "127.0.0.1",
+      port: 7545,
       network_id: "*" // Match any network id
     }
   },
