b72591c82c2d8935f97974b066726d0cdbb5ce06
Mark Mavromatis <markmavromatis@gmail.com>
2023-01-01 15:39:59 -0600

    Added wallet provider library, integrate with Goerli test network

--- project-6/truffle.js
+++ project-6/truffle.js
@@ -1,3 +1,7 @@
+const mnemonic = "pizza reveal during fame wife vendor sustain start close where trade evoke";
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const infuraKey = "<INFURA KEY GOES HERE>";
+
 module.exports = {
   networks: {
     development: {
@@ -9,7 +13,12 @@ module.exports = {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*" // Match any network id
-    }
+    },
+    goerli: {
+      provider: () => new HDWalletProvider(mnemonic, `https://goerli.infura.io/v3/${infuraKey}`),
+        network_id: 5,       // goerli's id
+        gasPrice: 25000000,
+    },
   },
   compilers: {
     solc: {
